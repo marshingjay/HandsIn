568ac9dd7a86be2af6471ce56baa4d92c0ffdd60
cmeyer <artgirlie26@gmail.com>
2020-01-26 10:15:32 -0600

    front and back

---
+++ .next/static/development/pages/creators.js
@@ -0,0 +1,94 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/creators.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcreators&absolutePagePath=%2FUsers%2Fclaremeyer%2FDocuments%2Fspring_senior%2Fsenior_design%2FhandsIn%2Fpages%2Fcreators.js!./":
+/*!**********************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcreators&absolutePagePath=%2FUsers%2Fclaremeyer%2FDocuments%2Fspring_senior%2Fsenior_design%2FhandsIn%2Fpages%2Fcreators.js ***!
+  \**********************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/creators", function() {
+      var mod = __webpack_require__(/*! ./pages/creators.js */ "./pages/creators.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/creators.js */ "./pages/creators.js", function() {
+          if(!next.router.components["/creators"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/creators.js */ "./pages/creators.js")
+          next.router.update("/creators", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_5f137288facb1107b491 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_5f137288facb1107b491 */ "dll-reference dll_5f137288facb1107b491"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/creators.js":
+/*!***************************!*\
+  !*** ./pages/creators.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Creators; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/claremeyer/Documents/spring_senior/senior_design/handsIn/pages/creators.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+function Creators() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "This will be about us"));
+}
+
+/***/ }),
+
+/***/ 2:
+/*!**************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fcreators&absolutePagePath=%2FUsers%2Fclaremeyer%2FDocuments%2Fspring_senior%2Fsenior_design%2FhandsIn%2Fpages%2Fcreators.js ***!
+  \**************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fcreators&absolutePagePath=%2FUsers%2Fclaremeyer%2FDocuments%2Fspring_senior%2Fsenior_design%2FhandsIn%2Fpages%2Fcreators.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcreators&absolutePagePath=%2FUsers%2Fclaremeyer%2FDocuments%2Fspring_senior%2Fsenior_design%2FhandsIn%2Fpages%2Fcreators.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_5f137288facb1107b491":
+/*!*******************************************!*\
+  !*** external "dll_5f137288facb1107b491" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_5f137288facb1107b491;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=creators.js.map
---
+++ front/pages/creators.js
@@ -0,0 +1,7 @@
+export default function Creators() {
+    return (
+        <div>
+            <p>This will be about us</p>
+        </div>
+    );
+}
