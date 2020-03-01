5a6faff8bec16ce5c8bb588f517f0d6985de1446
marshingjay <jam605@gmail.com>
2020-03-01 09:47:28 -0600

    api skeleton

---
+++ front/pages/api/events/[event].js
@@ -0,0 +1,10 @@
+
+
+export default (req, res) => {
+    const {
+        query: {event},
+    } = req
+
+    // do database query given event, which corresponds to the event key...
+    //return an event object storing all the relevant information 
+}
